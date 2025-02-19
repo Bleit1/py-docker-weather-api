8bd69f971aeeb5df10fdd31529fed6348d21b7b1
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-02-19 19:05:32 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = "https://api.weatherapi.com/v1/current.json?"
+    filtering = "Kyiv"
+
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("The API_KEY must be set!")
+
+    response = requests.get(url + f"key={api_key}&q={filtering}")
+    data = response.json()
+
+    city = data["location"]["name"]
+    country = data["location"]["country"]
+    local_time = data["location"]["localtime"]
+    temp = data["current"]["temp_c"]
+    weather = data["current"]["condition"]["text"]
+
+    print(f"{city}/{country} {local_time} Weather: {temp} Celsius, {weather}")
 
 
 if __name__ == "__main__":
